a30e360039fd75d7d6b28265f671876c12f1db42
mhavela <mika.havela@gmail.com>
2024-10-27 22:21:52 +0100

    Disable Jerk, enable JD

--- config/examples/Creality/CR-10S Pro/Configuration.h
+++ config/examples/Creality/CR-10S Pro/Configuration.h
@@ -1340,7 +1340,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define CLASSIC_JERK
+//#define CLASSIC_JERK  //MH I choose Junction Deviation
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK  5.0
